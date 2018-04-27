9f85262eece0db071f835b3eb9bcbbfad0575f44
iosLiuPeng <392009255@qq.com>
2018-04-27 15:24:53 +0800

    Initial Commit

---
+++ MJSwitchView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MJSwitchView
+//
+//  Created by 刘鹏 on 2018/4/27.
+//  Copyright © 2018年 musjoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MJSwitchView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MJSwitchView
+//
+//  Created by 刘鹏 on 2018/4/27.
+//  Copyright © 2018年 musjoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
